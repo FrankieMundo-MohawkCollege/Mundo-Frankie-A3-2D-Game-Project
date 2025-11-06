1aac7f1058242675a38776e54b788c31e6576437
FrankieMundo-MohawkCollege <frank.mundo3@mohawkcollege.ca>
2025-11-06 00:11:51 -0500

    Finsihed Player.cs.

--- Mundo-Frankie-A3-2D-Game-Project/Player.cs
+++ Mundo-Frankie-A3-2D-Game-Project/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Raylib_cs;
+using System;
 using System.Numerics;
 
 namespace MohawkGame2D
@@ -7,8 +8,9 @@ namespace MohawkGame2D
     {
         Vector2 position;
         Vector2 velocity = new Vector2 (0, 0);
-        float gravity = 0.6f;
-        bool isOnGround = false;
+        Vector2 size = new Vector2 (50, 50);
+        float gravity = 200f;
+        bool isOnGround;
         public void Setup()
         {
 
@@ -16,36 +18,71 @@ namespace MohawkGame2D
 
         public void Update()
         {
+            SimulateGravity();
             Physics();
+            inputs();
             DrawPlayer();
+            
         }
 
         void Physics()
         {
-            velocity.Y += gravity;
-            position.X += velocity.X;
+            float playerTop = position.Y;
+            float playerBottom = position.Y + size.Y;
+            float playerLeft = position.X;
+            float playerRight = position.X + size.X;
 
-            if (position.Y > 300)
+            
+            if (playerTop <= 0)
             {
-                position.Y = 300;
-                velocity.Y = 0;
+                position.Y = 0;
+                velocity.Y = 0f;
+            }
+
+            if (playerBottom >= 300)
+            {
+                position.Y = 300 - size.Y;
+                velocity.Y = 0f;
                 isOnGround = true;
             }
-            else
-            {
-                isOnGround = false;
-            }
 
-            if (isOnGround)
-            {
-                velocity.Y = -10;
-            }
+            if (playerLeft <= 0)
+                position.X = 0;
+
+            if (playerRight >= 800)
+                position.X = 800 - size.X;
+
         }      
 
         void DrawPlayer()
         {
-            Draw.FillColor = Color.Red;
-            Draw.Rectangle(position.X, position.Y, 50, 50);
+            Draw.FillColor = Color.Magenta;
+            Draw.Rectangle (position, size);
+        }
+
+        void inputs()
+        {
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space) && isOnGround)
+            {
+                velocity.Y -= 100;
+            }
+
+            if (Input.IsKeyboardKeyDown(KeyboardInput.A))
+            {
+                position.X -= 2;
+            }
+            if (Input.IsKeyboardKeyDown(KeyboardInput.D))
+            {
+                position.X += 2;
+            }
+        }
+
+        public void SimulateGravity()
+        {
+            
+            velocity.Y += gravity * Time.DeltaTime;
+            
+            position.Y += velocity.Y * Time.DeltaTime;
         }
 
     }
